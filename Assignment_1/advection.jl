1276879a93db253042a04cbe3d5f91a09e7bf9b3
FedericoRossi <fede.rossi1999@gmail.com>
2024-02-11 15:58:19 +0100

    Advection completed

--- Assignment_1/advection.jl
+++ Assignment_1/advection.jl
@@ -1,5 +1,5 @@
 using LinearAlgebra
-import Pkg; Pkg.add("Plots")
+# import Pkg; Pkg.add("Plots")
 using Plots
 using SpecialFunctions
 
@@ -25,7 +25,7 @@ function dirichlet(N, α, start_u, steps)
     for _ in 1:steps
         u_n = C * u_n
       #  check_conservation(u_n, N)
-        # println(u_n)
+        println(u_n)
     end
     plot(x, u_n, label="Dirichlet", show=true)
 end
@@ -83,7 +83,7 @@ function neumann_space_dependent(N, α_vec, start_u, steps)
     x = [i / (N + 1) for i in 1:N]
     u_n = zeros(N)
     u_n[Int((N + 1) / 2)] = start_u
-    # println(u_n)
+    println(u_n)
 
     alpha_halved_1 = α_vec[1:end-1] / 2
     alpha_halved_1[1] = α_vec[1]
@@ -171,36 +171,78 @@ function A_minus(t, x0, D_plus, D_minus)
     return result * exp((D_plus - D_minus) * x0^2 / (4 * D_minus * D_plus * t))
 end
 
+function init_u(x)
+    return max(1-16*(x-0.5)^2,0)
+end
 
-dt = 0.0001
-N = 511
-start_u = 1.0
-D = 1
-D_plus = 1.6
-D_minus = 0.8
-L = 1.0
+function update_u(u, γ)
+    updated_u = (1-γ^2)*u
+    updated_u[1:N-1] += γ/2*(γ-1)*u[2:N]
+    updated_u[N] += γ/2*(γ-1)*u[1] #This to have u(0)=u(1)
+    updated_u[2:N] +=  γ/2*(γ+1)*u[1:N-1]
+    updated_u[1] +=  γ/2*(γ+1)*u[N] #This to have u(0)=u(1)
+
+    return updated_u
+end
+
+function init_u_gauss(x)
+    σ = 0.02
+    return ( sqrt(σ * pi)) * exp(-(x - 0.5)^2 / σ)
+end
+
+function update_u_hopf(u, γ)
+    updated_u = u
+    for n in 2:N-1
+        updated_u[n] -= γ/4*(u[n+1]^2-u[n-1]^2)  
+        updated_u[n] += γ^2/8*(u[n+1]+u[n])*(u[n+1]^2-u[n]^2)
+        updated_u[n] -= γ^2/8*(u[n]+u[n-1])*(u[n]^2-u[n-1]^2) 
+    end
+
+    return updated_u
+end
+
+
+dt = 0.01
+N = 85
 x = [i / (N + 1) for i in 1:N]
-x0 = L / 2
 @show dx = 1 / (N + 1)
-α = D * dt / (dx * dx)
-α_vec = dt / (dx * dx) * [eval_D(x_i, x0, D_plus, D_minus) for x_i in x]
+@show γ = dt / dx
 
-n_steps = 1000
+n_steps = 500
 
-dirichlet(N, α, start_u / dx, n_steps)
-neumann(N, α, start_u / dx, n_steps)
-γ = 4 * D * dt * n_steps
-unbound = [gaussian_unbound(x_i, x0, γ, start_u) for x_i in x]
+u = [init_u_gauss(x_i) for x_i in x]
 
-display(plot!(x, unbound, label="Unbound_exact", show=true))
+anim = @animate for k in 1:n_steps
+    global u = update_u_hopf(u,γ)
+    if k%1==0
+        plot(x, u, ylim=(0.0, 0.3), label="Lax-Wendroff", title="Step: $k", show=true)
+    end
+end
+gif(anim, "/home/frossi/ComputationalPhysics/Assignment_1/Lax_wendroff.gif", fps=100)
 
-exact_reflective = [analytical_solution(x_i, x0, γ, start_u, L, true) for x_i in x]
-plot(x, exact_reflective, label="Exact_reflective", show=true)
-exact_absorbing = [analytical_solution(x_i, x0, γ, start_u, L, false) for x_i in x]
-display(plot!(x, exact_absorbing, label="Exact_absorbing", show=true))
+# display(plot(x, u, label="Lax-Wendroff", show=true))
 
-dirichlet_space_dependent(N, α_vec, start_u/dx, n_steps)
-neumann_space_dependent(N, α_vec, start_u/dx, n_steps)
 
-unbound_sp_dependent = [start_u* unbounded_space_dependent(x_i, x0, dt*n_steps, D_plus, D_minus) for x_i in x]
-display(plot!(x, unbound_sp_dependent, label="Unbound_dependent", show=true))
+
+
+
+
+
+
+# dirichlet(N, α, start_u / dx, n_steps)
+# neumann(N, α, start_u / dx, n_steps)
+# γ = 4 * D * dt * n_steps
+# unbound = [gaussian_unbound(x_i, x0, γ, start_u) for x_i in x]
+
+# display(plot!(x, unbound, label="Unbound_exact", show=true))
+
+# exact_reflective = [analytical_solution(x_i, x0, γ, start_u, L, true) for x_i in x]
+# plot(x, exact_reflective, label="Exact_reflective", show=true)
+# exact_absorbing = [analytical_solution(x_i, x0, γ, start_u, L, false) for x_i in x]
+# display(plot!(x, exact_absorbing, label="Exact_absorbing", show=true))
+
+# dirichlet_space_dependent(N, α_vec, start_u/dx, n_steps)
+# neumann_space_dependent(N, α_vec, start_u/dx, n_steps)
+
+# unbound_sp_dependent = [start_u* unbounded_space_dependent(x_i, x0, dt*n_steps, D_plus, D_minus) for x_i in x]
+# display(plot!(x, unbound_sp_dependent, label="Unbound_dependent", show=true))

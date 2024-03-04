3d1203e6ea2ac6d1c092c0fe202649f24bde0241
FedericoRossi <fede.rossi1999@gmail.com>
2024-03-04 17:51:18 +0100

    Improved printing and gifs

--- Assignment_2/SE_solution.jl
+++ Assignment_2/SE_solution.jl
@@ -1,6 +1,8 @@
 using LinearAlgebra
 using Plots
 using SpecialFunctions
+using LaTeXStrings
+using Printf
 
 #####################
 # PARTICLE IN A BOX #
@@ -8,9 +10,9 @@ using SpecialFunctions
 
 function solution_FDM(N,dx)
     H = SymTridiagonal(2*ones(N), -ones(N-1)) / (dx^2)
-    display(H)
+    # display(H)
     lambda_vec = eigvals(H)
-    @show psi_vec = eigvecs(H)
+    psi_vec = eigvecs(H)
     for n in 1:N
         psi_vec[:,n] = psi_vec[:,n] / L2_integral(psi_vec[:,n], dx)
     end
@@ -51,23 +53,30 @@ lambda_exact, psi_exact = solution_exact(N-1, dx)
 # Plotting first 3 eigenfuntions
 plotting=[2,3,4]
 labels=["Numerical ψ1" "Numerical ψ2" "Numerical ψ3"]
-plot(x_vec, psi_vec[:,plotting], label=labels)
+plot(x_vec, psi_vec[:,plotting], label=labels, xlabel="x", ylabel="ψ(x)", title="First 3 eigenfunctions")
 labels=["Exact ψ1" "Exact ψ2" "Exact ψ3"]
 display(plot!(x_vec, psi_exact[:,plotting], label=labels))
-# display(plot(x_vec,  psi_exact[:,2].* psi_exact[:,4], label=labels))
+labels=[L"ψ_1" L"ψ_2" L"ψ_3"]
+display(plot(x_vec, psi_exact[:,plotting], label=labels, xlabel=L"x", ylabel=L"ψ(x)", title="First 3 eigenfunctions "*L"ψ_1 \, ψ_2 \, ψ_3"))
 
 
-println(L2_integral(psi_exact[:,1], dx))
-println(L2_integral(psi_vec[:,1],dx))
+println("L2 norm of psi_exact_1 : ", L2_integral(psi_exact[:,2], dx))
+println("L2 norm of psi_numerical_1 : ", L2_integral(psi_vec[:,2],dx))
 
 #This to fix the sign
-println(min(L2_integral(psi_exact[:,1]-psi_vec[:,1], dx), L2_integral(psi_exact[:,1]+psi_vec[:,1], dx)))
-println(min(L2_integral(psi_exact[:,2]-psi_vec[:,2], dx), L2_integral(psi_exact[:,2]+psi_vec[:,2], dx)))
-println(min(L2_integral(psi_exact[:,3]-psi_vec[:,3], dx), L2_integral(psi_exact[:,3]+psi_vec[:,3], dx)))
+# println(min(L2_integral(psi_exact[:,1]-psi_vec[:,1], dx), L2_integral(psi_exact[:,1]+psi_vec[:,1], dx)))
+println("L2 norm of psi_exact_1 - psi_numerical_1 : ", min(L2_integral(psi_exact[:,2]-psi_vec[:,2], dx), L2_integral(psi_exact[:,2]+psi_vec[:,2], dx)))
+println("L2 norm of psi_exact_2 - psi_numerical_2 : ", min(L2_integral(psi_exact[:,3]-psi_vec[:,3], dx), L2_integral(psi_exact[:,3]+psi_vec[:,3], dx)))
 
 # Plotting eigenval in function of n
-plot(n_vec, lambda_exact, label="Exact", show=true)
-display(plot!(n_vec, lambda_vec, label="Numerical", show=true))
+plot(n_vec, lambda_exact, xlabel=L"n", ylabel=L"λ_n", label=L"λ_n"*" exact", title="Comparison of eigenvalues as function of "*L"n")
+display(plot!(n_vec, lambda_vec, label=L"λ_n"*" numerical"))
+display(plot(n_vec, (lambda_exact.-lambda_vec)./lambda_exact, label=L"λ_n"*" relative difference"))
+
+
+# Plotting numerical vs exact_solution
+display(plot(lambda_exact, lambda_vec, label="Numerical"))
+
 
 
 
@@ -142,20 +151,30 @@ display(overlap)
 # Time evolution from ψ_1
 psi_0 = initialize_from_psi1_exact(N, dx)
 println("Norm of Ψ_0 as ψ_1: ", L2_integral(psi_0, dx))
-alpha_n = [integrate_product(psi_exact[:,i], psi_0, N, dx) for i in 2:N]
+alpha_n = [integrate_product(psi_vec[:,i], psi_0, N, dx) for i in 2:N]
 println("α_n: ",  alpha_n)
-evoluted_Psi = time_evolution(alpha_n, psi_exact, lambda_exact, 1, N)
-println("Norm of Ψ(t)", L2_integral(evoluted_Psi, dx))
+evoluted_Psi = time_evolution(alpha_n, psi_exact, lambda_exact, pi/lambda_exact[1], N)
+println("Norm of Ψ(t): ", L2_integral(evoluted_Psi, dx))
+module_Psi = [conj(psi_i)*psi_i for psi_i in evoluted_Psi]
+display(plot(x_vec, real.(module_Psi), label=labels, xlabel=L"x", ylabel=L"ψ(x)", title="First 3 eigenfunctions "*L"ψ_1 \, ψ_2 \, ψ_3"))
+
 
 # Time evolution from δ(x' - 0.5)
-initialize_from_delta(N, dx)
+psi_0 = initialize_from_delta(N, dx)
 println("Norm of Ψ_0 as delta: ", L2_integral(psi_0, dx))
 alpha_n = [integrate_product(psi_exact[:,i], psi_0, N, dx) for i in 2:N]
 println("α_n: ",  alpha_n)
 @show evoluted_Psi = time_evolution(alpha_n, psi_exact, lambda_exact, 1, N)
 println("Norm of Ψ(t)", L2_integral(evoluted_Psi, dx))
 # I have no idea what happens for t>0
-
+if false
+    anim = @animate for k in 1:200
+        evoluted_Psi = time_evolution(alpha_n, psi_exact, lambda_exact, k/100000, N) #t_bar*(1-0.01*(k-50))
+        module_Psi = [conj(psi_i)*psi_i for psi_i in evoluted_Psi]
+        plot(x_vec, real.(module_Psi), label=L"|Ψ|^2", ylim=(-0.05, 10.1), xlabel=L"x", ylabel=L"|Ψ|^2(x)", title="Evolution of "*L"δ(x-0.5)"*@sprintf " at time %.00005f" k/100000)
+    end
+    display(gif(anim, "/home/frossi/ComputationalPhysics/Assignment_2/Time_evolution_delta.gif", fps=60))
+end
 
 
 
@@ -238,11 +257,18 @@ lambda_barrier, psi_barrier = solution_FDM_box(V_n, N-1, dx)
 v0 = 1e3
 V_n = potential_barrier(v0, N, dx)
 lambda_barrier, psi_barrier = solution_FDM_box(V_n, N-1, dx)
+println("First 10 λ_n from box with barrier of v_0 = $v0 : ", lambda_barrier[1:10])
+println("Last 10 λ_n from box with barrier of v_0 = $v0 : ", lambda_barrier[90:99])
+plot(1:20, lambda_exact[1:20], xlabel=L"n", ylabel=L"λ_n", label=L"λ_n, v_0 = 0"*", exact", marker = :circle, title="First 20 eigenvalues depending on "*L"n")
+plot!(1:20, lambda_vec[1:20], label=L"λ_n, v_0 = 0", marker = :circle)
+display(plot!(1:20, lambda_barrier[1:20], label=L"λ_n, v_0 = 10^3", marker = :circle))
+display(plot(2:N-1, (lambda_barrier[2:end].-lambda_barrier[1:end-1])./lambda_barrier[2:end], marker = :circle,))
+
 # Plotting first 3 eigenfunctions
-plotting=[1,2,3]
-labels=["Numerical ψ1" "Numerical ψ2" "Numerical ψ3"]
-display(plot(x_vec, psi_barrier[:,plotting], label=labels))
-println(lambda_barrier)
+plotting=[2,3,8]
+labels=["Numerical "*L"ψ_1" "Numerical "*L"ψ_2" "Numerical "*L"ψ_7"]
+display(plot(x_vec, psi_barrier[:,plotting], xlabel=L"x", ylabel=L"ψ(x)", label=labels, title="Selection of eigenfunctions with "*L"v_0 = 10^3"))
+# println(lambda_barrier)
 #### NOTE ####
 # The eigenvalues grow up in couples 
 psi_0 = initialize_from_superposition(psi_barrier)
@@ -323,19 +349,14 @@ function double_potential_barrier(V0, V1, N, dx)
     return V_n
 end
 
-λ1_list = []
-λ2_list = []
-v_list = []
-for exp in -40:40
-    local V_n = double_potential_barrier(v0, sign(exp)*10^(abs(exp)/12), N, dx)
-    lambda_double, psi_double = solution_FDM_box(V_n, N-1, dx)
-    push!(v_list,  sign(exp)*10^(abs(exp)/12))
-    push!(λ1_list, lambda_double[1])
-    push!(λ2_list, lambda_double[2])
-end
-plot(v_list, λ1_list, xlabel="V1", label="λ1", title="Eigenvalues depending on V1")
-display(plot!(v_list, λ2_list, label="λ2"))
+function evaluate_τ(V_n, ψ, N, dx)
+    H = SymTridiagonal(2*ones(N), -ones(N-1)) / (dx^2)
+    H = H + diagm(V_n)
+    ψ_test = zeros(N+2)
+    ψ_test[2:end-1] = H*ψ[2:end-1,3]
 
+    return integrate_product(ψ[:,2], ψ_test, N, dx)
+end
 
 
 λ1_list = []
@@ -345,22 +366,36 @@ v_list = []
 plotting = [2,3]
 labels = ["ψ1" "ψ2"]
 
-for exp in -500:500
-    local V_n = double_potential_barrier(v0, exp, N, dx)
+for v1 in -500:500
+    local V_n = double_potential_barrier(v0, v1, N, dx)
     lambda_double, psi_double = solution_FDM_box(V_n, N-1, dx)
-    push!(v_list,  exp)
+    push!(v_list,  v1)
     push!(λ1_list, lambda_double[1])
     push!(λ2_list, lambda_double[2])
-    if exp == -500
+    if v1 == -500
         display(plot(x_vec, psi_double[:,plotting], label=labels, title="First eigenfunctions at V1 = -500"))
-    elseif exp == 500
+    elseif v1 == 500
         display(plot(x_vec, psi_double[:,plotting], label=labels, title="First eigenfunctions at V1 = 500"))
-    elseif exp == 0
+    elseif v1 == 0
         println("ΔE at V1=0 : ", lambda_double[2]-lambda_double[1])
     end
 end
 
-plot(v_list, λ1_list, xlabel="V1", label="λ1", title="Eigenvalues depending on V1")
-display(plot!(v_list, λ2_list, label="λ2"))
+plot(v_list, λ1_list, xlabel="V1", label=" λ0", title="Eigenvalues depending on V1")
+display(plot!(v_list, λ2_list, label="λ1"))
 
-display(plot(v_list, λ2_list .- λ1_list, xlabel="V1", label="λ1", title="Eigenvalues difference depending on V1"))
+display(plot(v_list, λ2_list .- λ1_list, xlabel="V1", label="λ1-λ0", title="Eigenvalues difference depending on V1"))
+
+# Finding τ
+V_n = double_potential_barrier(v0, 0.0, N, dx)
+lambda_ref, psi_ref = solution_FDM_box(V_n, N-1, dx)
+println("τ with v1 = 0 : ", evaluate_τ(V_n, psi_ref, N-1, dx))
+
+τ_list = []
+v_list = []
+for v1 in -500:500
+    local V_n = double_potential_barrier(v0, v1, N, dx)
+    push!(τ_list, evaluate_τ(V_n, psi_ref, N-1, dx))
+    push!(v_list,  v1)
+end
+display(plot(v_list, τ_list, xlabel="V1", label="τ(V1)", title="τ depending on V1"))
